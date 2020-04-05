cc082ef0e90097b37c32f2f29a14aeb4291d66e4
Merike11 <merike.toose@ametikool.ee>
2020-04-05 16:44:50 +0300

    Kustuta töötab

---
+++ delete.php
@@ -0,0 +1,66 @@
+<?php
+
+require "./config.php";
+require "./common.php";
+
+if (isset($_GET["id"])) {
+    try {
+        $connection = new PDO($dsn, $username, $password, $options);
+        $id = $_GET["id"];
+
+        $sql = "DELETE FROM markers.markers WHERE id = :id";
+            
+        $statement = $connection->prepare($sql);
+        $statement->bindValue(':id', $id);
+        $statement->execute();
+            
+        $success = "Marker edukalt kustutatud";
+  } catch(PDOException $error) {
+  echo $sql . "<br>" . $error->getMessage();
+  }
+}
+
+try {
+    $connection = new PDO($dsn, $username, $password, $options);
+  
+    $sql = "SELECT * FROM markers.markers";
+  
+    $statement = $connection->prepare($sql);
+    $statement->execute();
+  
+    $result = $statement->fetchAll();
+  } catch(PDOException $error) {
+    echo $sql . "<br>" . $error->getMessage();
+  }
+?>
+
+<h2>Kustuta markerid</h2>
+
+<table>
+    <thead>
+        <tr>
+            <th>Nr</th>
+            <th>Nimi</th>
+            <th>Pikkuskraad</th>
+            <th>Laiuskraad</th>
+            <th>Kirjeldus</th>
+            <th>Lisatud</th>
+            <th>Kustutatud</th>
+        </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($result as $row) : ?>
+        <tr>
+          <td><?php echo escape($row["id"]); ?></td>
+          <td><?php echo escape($row["name"]); ?></td>
+          <td><?php echo escape($row["latitude"]); ?></td>
+          <td><?php echo escape($row["longitude"]); ?></td>
+          <td><?php echo escape($row["description"]); ?></td>
+          <td><?php echo escape($row["added"]); ?></td>
+          <td><a href="delete.php?id=<?php echo escape($row["id"]); ?>">Kustuta</a></td>
+        </tr>
+    <?php endforeach; ?>
+    </tbody>
+</table>
+  
+<a href="index.php">Tagasi algusesse</a>
--- update-single.php
+++ update-single.php
@@ -1,18 +1,69 @@
 <?php
 
-/**
-  * Use an HTML form to edit an entry in the
-  * users table.
-  *
-  */
-
 require "./config.php";
 require "./common.php";
 
+if (isset($_POST['submit'])) {
+  try {
+    $connection = new PDO($dsn, $username, $password, $options);
+    $marker =[
+      "id"        => $_POST['id'],
+      "name" => $_POST['name'],
+      "latitude"  => $_POST['latitude'],
+      "longitude"     => $_POST['longitude'],
+      "desription"       => $_POST['description'],
+      "added"  => $_POST['added'],
+      "edited"      => $_POST['edited']
+    ];
+
+    $sql = "UPDATE markers.markers
+            SET id = :id,
+              name = :name,
+              latitude = :latitude,
+              longitude = :longitude,
+              description = :description,
+              added = :added,
+              edited = :edited
+            WHERE id = :id";
+
+  $statement = $connection->prepare($sql);
+  $statement->execute($user);
+  } catch(PDOException $error) {
+      echo $sql . "<br>" . $error->getMessage();
+  }
+}
+
 if (isset($_GET['id'])) {
-  echo $_GET['id']; // for testing purposes
+  try {
+    $connection = new PDO($dsn, $username, $password, $options);
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM markers.markers WHERE id = :id";
+    $statement = $connection->prepare($sql);
+    $statement->bindValue(':id', $id);
+    $statement->execute();
+
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+  } catch(PDOException $error) {
+      echo $sql . "<br>" . $error->getMessage();
+  }
 } else {
     echo "Something went wrong!";
     exit;
 }
 ?>
+
+<?php if (isset($_POST['submit']) && $statement) : ?>
+  <?php echo escape($_POST['name']); ?> successfully updated.
+<?php endif; ?>
+
+<h2>Muuda marker</h2>
+
+<form method="post">
+    <?php foreach ($user as $key => $value) : ?>
+      <label for="<?php echo $key; ?>"><?php echo ucfirst($key); ?></label>
+      <input type="text" name="<?php echo $key; ?>" id="<?php echo $key; ?>" value="<?php echo escape($value); ?>" <?php echo ($key === 'id' ? 'readonly' : null); ?>>
+    <?php endforeach; ?>
+    <input type="submit" name="submit" value="Submit">
+</form>
+
+<a href="index.php">Back to home</a>
